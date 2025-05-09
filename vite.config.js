b441a83b6095c5ee2f6c866e444450e867414c9f
YourDevVince <vince.dev.help@gmail.com>
2025-05-09 04:19:48 -0500

    added base to vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/anime-quote-gen/',
   plugins: [react()],
   server: {
     port: 3001, // Runs Vite on this port
